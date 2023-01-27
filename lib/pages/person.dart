eef6b69b322c6b9a3fa479f21ae8a47c3fad9347
whl <1677378469@qq.com>
2023-01-27 13:08:16 +0800

    添加LayoutBuilder规范Wrap布局

--- lib/pages/person.dart
+++ lib/pages/person.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:wechat_assets_picker/wechat_assets_picker.dart';
 
 class PersonPage extends StatefulWidget {
   const PersonPage({Key? key}) : super(key: key);
@@ -8,11 +9,49 @@ class PersonPage extends StatefulWidget {
 }
 
 class _PersonPageState extends State<PersonPage> {
+  List<AssetEntity> picks = [];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("我的"),),
-      body: Container(),
+      appBar: AppBar(
+        title: const Text("我的"),
+      ),
+      body: buildBody(context),
+    );
+  }
+
+  Widget buildPhotoList(BuildContext context) {
+    return LayoutBuilder(builder: (context, cons) {
+      double width = (cons.maxWidth - 3 * 10) / 4;
+      return Wrap(spacing: 10, runSpacing: 10, children: [
+        for (final asset in picks)
+          AssetEntityImage(
+            asset,
+            width: width,
+            height: width,
+            fit: BoxFit.cover,
+          )
+      ]);
+    });
+  }
+
+  Widget buildBody(BuildContext context) {
+    return Column(
+      children: [
+        ElevatedButton(
+            onPressed: () async {
+              final results = await AssetPicker.pickAssets(context);
+              setState(() {
+                picks.clear();
+                picks.addAll(results!);
+              });
+            },
+            child: const Text("选取图片")),
+        Column(children: [
+          buildPhotoList(context),
+        ]),
+      ],
     );
   }
 }
